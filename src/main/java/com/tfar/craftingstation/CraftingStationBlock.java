867ac3e47d11c41aa3de663d9cf4f55f7cb01a16
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-08-04 16:37:10 -0400

    closes #10 , #8

--- src/main/java/com/tfar/craftingstation/CraftingStationBlock.java
+++ src/main/java/com/tfar/craftingstation/CraftingStationBlock.java
@@ -3,9 +3,12 @@ package com.tfar.craftingstation;
 import com.tfar.craftingstation.util.Helpers;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -17,12 +20,17 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class CraftingStationBlock extends Block {
 
   protected static final VoxelShape[] shapes;
   protected static final VoxelShape shape;
+
+  private static final Random RANDOM = new Random();
 
   public CraftingStationBlock(Properties p_i48440_1_) {
     super(p_i48440_1_);
@@ -71,8 +79,35 @@ static   {
   }
 
   @Override
-  public void onReplaced(BlockState p_196243_1_, World p_196243_2_, BlockPos p_196243_3_, BlockState p_196243_4_, boolean p_196243_5_) {
-    super.onReplaced(p_196243_1_, p_196243_2_, p_196243_3_, p_196243_4_, p_196243_5_);
+  public void onReplaced(BlockState state, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull BlockState newState, boolean isMoving) {
+    if (state.getBlock() != newState.getBlock()) {
+      TileEntity tileentity = worldIn.getTileEntity(pos);
+      if (tileentity instanceof CraftingStationTile) {
+        dropItems((CraftingStationTile)tileentity,worldIn, pos);
+        worldIn.updateComparatorOutputLevel(pos, this);
+      }
+      super.onReplaced(state, worldIn, pos, newState, isMoving);
+    }
+  }
+
+  public static void dropItems(CraftingStationTile table, World world, BlockPos pos) {
+    IntStream.range(0, table.input.getSlots()).mapToObj(i -> table.input.getStackInSlot(i)).filter(stack -> !stack.isEmpty()).forEach(stack -> spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack));
+  }
+
+  public static void spawnItemStack(World worldIn, double x, double y, double z, ItemStack stack) {
+    double d0 = EntityType.ITEM.getWidth();
+    double d1 = 1 - d0;
+    double d2 = d0 / 2;
+    double d3 = Math.floor(x) + RANDOM.nextDouble() * d1 + d2;
+    double d4 = Math.floor(y) + RANDOM.nextDouble() * d1;
+    double d5 = Math.floor(z) + RANDOM.nextDouble() * d1 + d2;
+
+    while(!stack.isEmpty()) {
+      ItemEntity itementity = new ItemEntity(worldIn, d3, d4, d5, stack.split(RANDOM.nextInt(21) + 10));
+      float f = 0.05F;
+      itementity.setMotion(RANDOM.nextGaussian() * f, RANDOM.nextGaussian() * f + 0.2, RANDOM.nextGaussian() * f);
+      worldIn.addEntity(itementity);
+    }
   }
 
   @Override
